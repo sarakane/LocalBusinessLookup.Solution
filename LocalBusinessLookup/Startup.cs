e6e0f3e6805c812b1e05d03436e8752a596caa4f
Sara Kane <kane.sarak@gmail.com>
2020-08-21 14:21:00 -0700

    Add swagger

--- LocalBusinessLookup/Startup.cs
+++ LocalBusinessLookup/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace LocalBusinessLookup
 {
@@ -23,6 +24,22 @@ namespace LocalBusinessLookup
             services.AddDbContext<LocalBusinessLookupContext>(opt =>
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Local Business Lookup API",
+                    Description = "An API to lookup local businesses",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sara Kane",
+                        Email = string.Empty,
+                        Url = new System.Uri("https://www.linkedin.com/in/sarakaneportland")
+                    }
+                });
+                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +54,13 @@ namespace LocalBusinessLookup
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Local Business Lookup API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
