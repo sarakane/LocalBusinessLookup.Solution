fe1bf96a9995888cba46fc364cab289ccbf0a010
Sara Kane <kane.sarak@gmail.com>
2020-08-21 11:08:18 -0700

    Add seed data for restaurants

---
+++ LocalBusinessLookup/Migrations/20200821180601_RestaurantSeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusinessLookup.Migrations
+{
+    public partial class RestaurantSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "RestaurantId", "Address", "City", "Cuisine", "Name", "PhoneNumber", "State" },
+                values: new object[,]
+                {
+                    { 1, "203 Villa Rd", "Newberg", "American(Traditional)", "Newbergundian Bistro", "971-832-8687", "OR" },
+                    { 2, "910 E 1st St", "Newberg", "Thai, Food Tucks", "Bangkok Noi Thai", "503-726-6067", "OR" },
+                    { 3, "808 E Hancock St", "Newberg", "Coffee & Tea, Bakeries", "Coffee Cottage", "503-538-5126", "OR" },
+                    { 4, "3777 Portland Rd", "Newberg", "Russian, Coffee & Tea", "From Russia With Love", "971-832-8579", "OR" },
+                    { 5, "155 SW 7th St", "Dundee", "Sandwiches, Pizza", "Red Hills Market", "971-832-8414", "OR" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 5);
+        }
+    }
+}
--- LocalBusinessLookup/Models/LocalBusinessLookupContext.cs
+++ LocalBusinessLookup/Models/LocalBusinessLookupContext.cs
@@ -11,5 +11,17 @@ namespace LocalBusinessLookup.Models
 
     public DbSet<Restaurant> Restaurants { get; set; }
     public DbSet<Shop> Shops { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Restaurant>()
+          .HasData(
+              new Restaurant { RestaurantId = 1, Name = "Newbergundian Bistro", Cuisine = "American(Traditional)", PhoneNumber = "971-832-8687", Address = "203 Villa Rd", City = "Newberg", State = "OR" },
+              new Restaurant { RestaurantId = 2, Name = "Bangkok Noi Thai", Cuisine = "Thai, Food Tucks", PhoneNumber = "503-726-6067", Address = "910 E 1st St", City = "Newberg", State = "OR" },
+              new Restaurant { RestaurantId = 3, Name = "Coffee Cottage", Cuisine = "Coffee & Tea, Bakeries", PhoneNumber = "503-538-5126", Address = "808 E Hancock St", City = "Newberg", State = "OR"  },
+              new Restaurant { RestaurantId = 4, Name = "From Russia With Love", Cuisine = "Russian, Coffee & Tea", PhoneNumber = "971-832-8579", Address = "3777 Portland Rd", City = "Newberg", State = "OR"  },
+              new Restaurant { RestaurantId = 5, Name = "Red Hills Market", Cuisine = "Sandwiches, Pizza", PhoneNumber = "971-832-8414", Address = "155 SW 7th St", City = "Dundee", State = "OR"  }
+          );
+    }
   }
 }
