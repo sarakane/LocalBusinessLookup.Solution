fd8f44cd6a1c742e7ed3b24a673bf04f06f56f30
Sara Kane <kane.sarak@gmail.com>
2020-08-21 13:31:42 -0700

    Add validations to restaurant and shop classes

--- LocalBusinessLookup/Models/Restaurant.cs
+++ LocalBusinessLookup/Models/Restaurant.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalBusinessLookup.Models
 {
   public class Restaurant
   {
     public int RestaurantId { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Cuisine {get; set; }
+    [Phone]
     public string PhoneNumber { get; set; }
     public string Address { get; set; }
     public string City { get; set; }
