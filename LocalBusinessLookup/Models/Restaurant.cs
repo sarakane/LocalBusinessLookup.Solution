0414fd0a8aea7a378c42a987f53c530ad29cae00
Sara Kane <kane.sarak@gmail.com>
2020-08-21 09:37:39 -0700

    Add restaurant and shop classes

---
+++ LocalBusinessLookup/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+namespace LocalBusinessLookup.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Cuisine {get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    
+  }
+}
---
+++ LocalBusinessLookup/Models/Shop.cs
@@ -0,0 +1,14 @@
+namespace LocalBusinessLookup.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string Name { get; set; }
+    public string Category {get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    
+  }
+}
